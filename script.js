b340a665acc444f1b5eb53b9aefbf8f0f7a0d41a
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-28 22:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 // Your code here.
+const slider = document.querySelector('.items');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Mouse down
+slider.addEventListener('mousedown', (e) => {
+
+  isDown = true;
+
+  slider.classList.add('active');
+
+  startX = e.pageX - slider.offsetLeft;
+
+  scrollLeft = slider.scrollLeft;
+});
+
+// Mouse leave
+slider.addEventListener('mouseleave', () => {
+
+  isDown = false;
+
+  slider.classList.remove('active');
+});
+
+// Mouse up
+slider.addEventListener('mouseup', () => {
+
+  isDown = false;
+
+  slider.classList.remove('active');
+});
+
+// Mouse move
+slider.addEventListener('mousemove', (e) => {
+
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+
+  const walk = (x - startX) * 2;
+
+  slider.scrollLeft = scrollLeft - walk;
+});
